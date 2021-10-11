8e786b98b46595dc91bde2ae2cfc7c73959fac51
Shameer <Muhammad_Shameer@epam.com>
2021-10-11 16:21:32 +0800

    added attributes to enum

--- src/main/java/com/epam/academy/common/Language.java
+++ src/main/java/com/epam/academy/common/Language.java
@@ -1,22 +1,32 @@
 package com.epam.academy.common;
 
 public enum Language {
-    JS("Java Script"),
-    JAVA("Java (Oracle)"),
-    KOTLIN("Kotlin ('NetBrains')"),
-    HTML("Hyper Text Markup Language"),
-    PYTHON("Python (the all in all)");
+    JS(true, true, "Java Script"),
+    JAVA(true, false, "Java (Oracle)"),
+    KOTLIN(true, false, "Kotlin ('NetBrains')"),
+    HTML(false, false, "Hyper Text Markup Language"),
+    PYTHON(true, true, "Python (the all in all)");
 
-    private String label;
+    private final boolean isProgrammingLanguage;
+    private final boolean canUseForScripting;
+    private final String qualifiedName;
 
-    Language() {
+
+    Language(boolean isProgrammingLanguage, boolean canUseForScripting, String qualifiedName) {
+        this.isProgrammingLanguage = isProgrammingLanguage;
+        this.canUseForScripting = canUseForScripting;
+        this.qualifiedName = qualifiedName;
     }
 
-    Language(String label) {
-        this.label = label;
+    public boolean isProgrammingLanguage() {
+        return isProgrammingLanguage;
     }
 
-    public String getLabel() {
-        return label;
+    public boolean canUseForScripting() {
+        return canUseForScripting;
+    }
+
+    public String getQualifiedName() {
+        return qualifiedName;
     }
 }
--- src/main/java/com/epam/academy/uglyCode/service/LanguageServiceImpl.java
+++ src/main/java/com/epam/academy/uglyCode/service/LanguageServiceImpl.java
@@ -3,32 +3,21 @@ package com.epam.academy.uglyCode.service;
 import com.epam.academy.common.Language;
 import com.epam.academy.common.LanguageService;
 
-import java.security.InvalidParameterException;
-
-import static com.epam.academy.common.Language.HTML;
-import static com.epam.academy.common.Language.JS;
-import static com.epam.academy.common.Language.PYTHON;
-
 
 public final class LanguageServiceImpl implements LanguageService {
 
     @Override
     public boolean isProgrammingLanguage(Language language) {
-        return language != HTML;
+        return language.isProgrammingLanguage();
     }
 
     @Override
     public boolean canUseForScripting(Language language) {
-        return language == JS || language == PYTHON;
+        return language.canUseForScripting();
     }
 
     @Override
-    public String getQualifiedName(Language language) throws InvalidParameterException {
-        if (language.getLabel() == null) {
-            throw new InvalidParameterException(String.format("Language %s is not defined",
-                        language.name()));
-        } else {
-            return language.getLabel();
-        }
+    public String getQualifiedName(Language language) {
+       return language.getQualifiedName();
     }
 }
